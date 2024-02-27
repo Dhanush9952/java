e63d726da7fb2ea7077cfdc3fd2e2beeb491feae
Dhanush <dhanushkrish54@gmail.com>
2024-02-27 18:22:26 +0530

    27-02-2024 | 06:22PM

---
+++ Swap2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Swap2{
+public static void main(String args[]){
+	int value1, value2;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter 2 numbers: ");
+System.out.println("Value 1: ");
+value1 = s.nextInt();
+System.out.println("Value 2: ");
+value2 = s.nextInt();
+value1 = value1 + value2;
+value2 = value1 - value2;
+value1 = value1 - value2;
+System.out.println("Swapped numbers are: ");
+System.out.println("Value 1: " + value1);
+System.out.println("Value 2: " + value2);
+}
+}
