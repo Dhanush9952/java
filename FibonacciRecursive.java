5343bfedd6414caa403d7ffdd94c6d0a706c1375
Dhanush <dhanushkrish54@gmail.com>
2024-03-12 18:59:34 +0530

    12-03-2024 | 06:59PM

--- FibonacciRecursive.java
+++ FibonacciRecursive.java
@@ -1,19 +1,14 @@
 import java.util.*;
 class FibonacciRecursive{
-	int x;
-	public int fibonacci(int x){
-	if(x==0){
-		return 0;
-	} else if(x==1){
-		return 1;
-	} else {
-		return (fibonacci(x-1) + fibonacci(x-2));
-	}
+	public int fibonacci(int n){
+	if(n <= 1) return 1; 
+	else return fibonacci(n-1) + fibonacci(n-2);
 }
 	public static void main(String args[]){
 		FibonacciRecursive fr = new FibonacciRecursive();
-		fr.x = Integer.parseInt(args[0]);
-		for(int i=0; i <= fr.x; i++){
+		Scanner x = new Scanner(System.in);
+		int num = x.nextInt();
+		for(int i=0; i <= num; i++){
 			System.out.println(fr.fibonacci(i));
 		}
 	}
