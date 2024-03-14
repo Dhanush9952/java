a352a4992ddf75de5d895506c0a70c75d105542a
Dhanush <dhanushkrish54@gmail.com>
2024-03-14 13:37:47 +0530

    14-03-2024 | 01:37PM

---
+++ HackerRank Problems/ReverseArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class ReverseArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int j=n-1; j>=0; j--){
+			System.out.print(arr[j] + " ");
+		}
+	}
+}
