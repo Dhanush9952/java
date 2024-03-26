953f2826fd2269d1ea934ef4283cd99f6bb8e5e9
Dhanush <dhanushkrish54@gmail.com>
2024-03-26 20:38:58 +0530

    26-03-2024|08:38PM

---
+++ Geeks Practice programs/IndexValue.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            ArrayList<Integer> ans = new Solution().valueEqualToIndex(arr, n);
+            if (ans.size() == 0) {
+                System.out.println("Not Found");
+            } else {
+                for (Integer x : ans) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class IndexValue {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == i + 1) {
+                res.add(i + 1); // Add i + 1, not just i
+            }
+        }
+        return res;
+    }
+}
