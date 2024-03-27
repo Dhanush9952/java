49db66bc7503eed40ae2f185e97ccfc9a2ddf7b1
Dhanush <dhanushkrish54@gmail.com>
2024-03-27 19:36:45 +0530

    test file update
    test file update 27-03-2024
    
    Co-Authored-By: Dhanush <87420521+Dhanush9952@users.noreply.github.com>

--- Test.java
+++ Test.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 class Test{
 	public static void main(String[] x){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int res = n/2;
-		System.out.println("n/2: " + res);		
-		int res2 = n % 2;
-		System.out.println("n%2: " + res2);
+				ArrayList<Integer> nums = new ArrayList<>();
+		for(int i=0; i<n; i++){
+			nums.add(sc.nextInt());
+		}
+		System.out.println(nums.get(0));
+		
 	}
 }
