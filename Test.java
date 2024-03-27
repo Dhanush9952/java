6e6b85e3efd5026bea05bb9d82b371be0c3c2b2e
Dhanush <dhanushkrish54@gmail.com>
2024-03-27 19:38:47 +0530

    27-03-2024
    test update
    
    Co-Authored-By: Dhanush <87420521+Dhanush9952@users.noreply.github.com>

--- Test.java
+++ Test.java
@@ -12,3 +12,18 @@ class Test{
 		
 	}
 }
+
+
+for (int i = 0; i < n; i++) {
+    int numberOfSwaps = 0;
+    for (int j = 0; j < n - 1; j++) {
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            numberOfSwaps++;
+        }
+    }
+   
+    if (numberOfSwaps == 0) {
+        break;
+    } 
+}
