8385a67fe9bd08d7d84f49eabb2ebed4f50bcd26
Dhanush <dhanushkrish54@gmail.com>
2024-03-29 20:20:23 +0530

    29-03-2024 | 08:20PM

--- Training/src/Practice/Pattern1.java
+++ Training/src/Practice/Pattern1.java
@@ -1,15 +1,15 @@
 package Practice;
 import java.util.Scanner;
 public class Pattern1 {
-public static void main(String[] x) {
+public static void main(String[] arg) {
 	Scanner scan = new Scanner(System.in);
 	System.out.println("Enter a number: ");
 	int N = scan.nextInt();
-	for(int i=0; i<N; i++) {
+	for(int i=0; i<=N; i++) {
 		for(int j=0; j<=i; j++) {
 			System.out.print("*");
 		}
-		System.out.println();		
+		System.out.println();
 	}
 }
 }
