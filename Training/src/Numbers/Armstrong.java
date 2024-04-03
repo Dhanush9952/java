0c1c682fd705936a02bdaa1b49fb9ad5d9525ae1
Dhanush <dhanushkrish54@gmail.com>
2024-04-03 12:52:06 +0530

    03-04-2024 | 12:52PM

--- Training/src/Numbers/Armstrong.java
+++ Training/src/Numbers/Armstrong.java
@@ -1,9 +1,6 @@
 package Numbers;
-
 import java.util.Scanner;
-
 public class Armstrong {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number:");
--- Training/src/Numbers/PrimeNumber.java
+++ Training/src/Numbers/PrimeNumber.java
@@ -1,7 +1,6 @@
 package Numbers;
 
 import java.util.Scanner;
-
 public class PrimeNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
